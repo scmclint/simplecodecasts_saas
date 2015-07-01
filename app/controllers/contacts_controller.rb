3e9a023c79a9aa9e06e5894f118f4f7be336d25a
Clint Schaeffer <neclint@yahoo.com>
2015-07-01 17:42:38 +0000

    added contact html and rails  code to controller file

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,7 @@
-class ContactController < ApplicationController
-  def new; end
+class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+  
   def create; end
 end
