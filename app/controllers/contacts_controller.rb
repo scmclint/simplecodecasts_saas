a0f52faf7b4a4129a5fcfc1a54c0e2a1eaa76274
Clint Schaeffer <neclint@yahoo.com>
2015-07-11 16:43:10 +0000

    Added validations to model and create contact action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,5 +3,21 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
   
-  def create; end
+  def create
+    @contact = Contact.new(contact_params)
+    
+    if @contact.save
+      flash[:success] = 'Message sent/saved.'
+      redirect_to new_contact_path
+    else
+      flash[:danger] = 'Error occured, message has not been sent/saved.'
+      redirect_to new_contact_path
+    end
+  end
+    
+    private
+      def contact_params
+        params.require(:contact).permit(:name, :email, :comments)
+      end
+  
 end
