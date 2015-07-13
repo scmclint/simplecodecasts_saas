1c810fc5c64ac108e883574eb57b9e7e29b9cc7a
Clint Schaeffer <neclint@yahoo.com>
2015-07-13 20:21:08 +0000

    deleted s from emails

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,7 @@ class ContactMailer < ActionMailer:: Base
   
   def contact_email(name, email, body)
     @name = name
-    @email = emails
+    @email = email
     @body = body
     
     mail(from: email, subject: 'Contact Form Message')
