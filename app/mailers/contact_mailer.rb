e13c8ca8102d7aad652f55908cbae728fe26f125
Clint Schaeffer <neclint@yahoo.com>
2015-07-15 15:10:28 +0000

    Removed the extra space between :: and Base in Class

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,4 +1,4 @@
-class ContactMailer < ActionMailer:: Base
+class ContactMailer < ActionMailer::Base
   default to: 'scmclint@gmail.com'
   
   def contact_email(name, email, body)
@@ -8,4 +8,6 @@ class ContactMailer < ActionMailer:: Base
     
     mail(from: email, subject: 'Contact Form Message')
   end
+
 end
+# end of ContactMailer class
