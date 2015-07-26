62630f73dc48d699417e87ac44f3c0b29fb7e981
Clint Schaeffer <neclint@yahoo.com>
2015-07-26 19:18:44 +0000

    Added javascript for Stripe, and fixed my screwups on the layout

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,10 +2,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     super do |resource|
       if params[:plan]
-        resource.plan_id = params[:plan] 
-        if resource.plan_id == 2   # User.first.plan_id = 2 (in DB)
+        resource.plan_id = params[:plan]
+        if resource.plan_id == 2
           resource.save_with_payment
-        else   # User.first.plan_id = 1 (in DB)
+        else
           resource.save
         end
       end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :plan
-  attr_accessor :stripe_card_token  # enables use of hidden field for strip
-  
+  attr_accessor :stripe_card_token
   def save_with_payment
     if valid?
-      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)  # stripe Gem creates a customer object, and charges the customer. Returns customer.id
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
       self.stripe_customer_token = customer.id
       save!
     end
