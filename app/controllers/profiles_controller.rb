06c6aafcb08378b5f623d2d99166185a03bffe30
Clint Schaeffer <neclint@yahoo.com>
2015-07-31 18:58:15 +0000

    Added form and profiles new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,17 @@
 class ProfilesController < ApplicationController
   public
 
+  def new
+    # Identify the user
+    @user = User.find(params[:user_id])
 
+    # Profile add form show up on page asscociation by routes.rb loop with user
+    @profile = @user.build_profile
+    
+    # allow user to fill out their own information
+    # save the information or cancel to avoid save
+    # 
+
+  end
+  
 end
