93384e6a10a2ca2a9eaf3ad26bba6dc7bdc2d658
Clint Schaeffer <neclint@yahoo.com>
2015-08-05 22:13:33 +0000

    Added form and create action for user profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,17 +1,26 @@
 class ProfilesController < ApplicationController
-  public
 
   def new
-    # Identify the user
-    @user = User.find(params[:user_id])
-
-    # Profile add form show up on page asscociation by routes.rb loop with user
+    @user = User.find( params[:user_id] )
     @profile = @user.build_profile
-    
-    # allow user to fill out their own information
-    # save the information or cancel to avoid save
-    # 
+  end  # end of new action
 
-  end
+  def create
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile(profile_params)
+    
+    if @profile.save
+      flash[:success] = "Profile Updated (into DB)"
+      redirect_to user_path(:user_id)
+    else  # nope didn't save successfully
+      render action: :new
+    end  # end of if @profile.save
+    
+  end  # end of create action
   
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end  # end of profile_params
+    
 end
